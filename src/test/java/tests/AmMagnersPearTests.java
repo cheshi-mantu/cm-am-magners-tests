3921fd46b54203400c08c900395a5ecfb62f098f
cheshi-mantu <cheshi.mantu@gmail.com>
2020-07-19 18:24:11 +0300

    General availability test + shop test

--- src/test/java/tests/AmMagnersPearTests.java
+++ src/test/java/tests/AmMagnersPearTests.java
@@ -1,6 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
 
@@ -10,6 +9,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 import static helpers.Environment.*;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Epic("Magners Pear availability test in AM")
 @Feature("Direct search from URL")
@@ -17,10 +17,11 @@ import static io.qameta.allure.Allure.step;
 @Tag("am_magners_tests")
 class AmMagnersPearTests extends TestBase {
     private String url;
+    private Boolean isAvailable = false;
     @Test
     @DisplayName("Search and check availability")
     @Description("1) build search string, \n 2) directly open search \n 3) check availability")
-    void searchAndCheckAvailiability() {
+    void searchAndCheckGeneralAvailiability() {
         step("PREP: building the search string", () -> {
             url = baseSearchUrl + searchString.replaceAll(" ", "+");
             System.out.println(url);
@@ -35,8 +36,49 @@ class AmMagnersPearTests extends TestBase {
             $(".dropdown__content.header-contacts__city-content").shouldHave(text("Ваш город Москва?"));
             $(byText("Да, верно")).click();
         });
-        step("CHECK: Checking search results page against search string", () -> {
-            $("div.catalog-list-item__container").shouldNotHave(text(searchString));
+        step("CHECK and PREP next text: isAvailable", () -> {
+            if ($("[data-name='Magners Pear']").exists()){
+                isAvailable = true;
+            }
+        });
+        step("CHECK and PREP next text: isAvailable", () -> {
+            assertTrue(isAvailable);
+        });
+    }
+    @Test
+    @DisplayName("Search and check availability at certain shop")
+    @Description("1) build search string, " +
+            "2) directly open search " +
+            "3) check availability" +
+            "4) click the product card" +
+            "5) ")
+    void searchTargetShop() {
+        step("PREP: building the search string", () -> {
+            url = baseSearchUrl + searchString.replaceAll(" ", "+");
+            System.out.println(url);
+        });
+        step ("PREP: Open " + url, () -> {
+            open(url);
+        });
+        step("PREP: Confirm we are older that 18", () -> {
+            $(byText("Подтверждаю, мне есть 18 лет")).click();
+        });
+        step("PREP: Yes, we are from Moscow", () -> {
+            $(".dropdown__content.header-contacts__city-content").shouldHave(text("Ваш город Москва?"));
+            $(byText("Да, верно")).click();
+        });
+        step("CHECK: search item is on the page", () -> {
+            $("[data-name='Magners Pear']").exists();
+        });
+        step("PREP: go to the item's page ", () -> {
+            $(".catalog-list-item__title.js-product-detail-link").click();
+        });
+        step("PREP: click availability button", () -> {
+            $(".btn.btn-primary.btn-outlined.catalog-element-info__shops.js-product-detail-stores-link").shouldHave(text("В наличии")).click();
+        });
+        step("PREP: enter search street name and Enter", () -> {
+            $(".search__input.js-store-search-input").setValue("Чертановская").pressEnter();
+            $("span.store-item__quantity").shouldHave(text("В наличии:"));
         });
     }
 
