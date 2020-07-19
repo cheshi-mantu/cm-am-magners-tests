9f2137f014afc16042429a20a31215d0b5b83df8
cheshi-mantu <cheshi.mantu@gmail.com>
2020-07-19 15:39:58 +0300

    Changed success to unavailaibility of the product

--- src/test/java/tests/AmMagnersPearTests.java
+++ src/test/java/tests/AmMagnersPearTests.java
@@ -25,48 +25,19 @@ class AmMagnersPearTests extends TestBase {
             url = baseSearchUrl + searchString.replaceAll(" ", "+");
             System.out.println(url);
         });
-        step ("Open " + url, () -> {
+        step ("ACT: Open " + url, () -> {
             open(url);
         });
-        step("Confirm we are older that 18", () -> {
+        step("PREP: Confirm we are older that 18", () -> {
             $(byText("Подтверждаю, мне есть 18 лет")).click();
         });
-        step("Yes, we are from Moscow", () -> {
+        step("PREP: Yes, we are from Moscow", () -> {
             $(".dropdown__content.header-contacts__city-content").shouldHave(text("Ваш город Москва?"));
             $(byText("Да, верно")).click();
         });
-//        step("Click \'Change your shipping country\'", () -> {
-//            $("[data-action='ShippingSwitcher']").click();
-//        });
-//        step("Click country selector, select country by list value => RU" +
-//                "Check if Currency is set to RUB", () -> {
-//            $(byId("gle_selectedCountry")).click();
-//            $(by("value", "RU")).click();
-//            $(byId("gle_selectedCurrency")).shouldHave(value("RUB"));
-//        });
-//        step("Click SAVE on country selector" +
-//                "check that country flag in the upper right corner is set" +
-//                "to Russian", () -> {
-//            $("[data-key='SavenClose']").click();
-//            $("#shippingSwitcherLink").shouldHave(cssClass("flag-ru"));
-//            $("ul.nav-container").shouldHave(text("Bags"));
-//        });
-//        step("Check if navigation bar has /'Bags/'", () -> {
-//            $("ul.nav-container").shouldHave(text("Bags"));
-//        });
-//        step("Hover the main menu over \'Bags\' laptop should be present in the drop down", () -> {
-//            $$("ul.nav-container li").findBy(text("Bags")).hover();
-//            $$("div.columns-4.sub-cat-menu-item.sub-cat-menu-list").find(text("laptop bags"));
-//        });
-//        step("Go to laptop bags collection, Vega should be present on the page", () -> {
-//            $("[data-gtm-link='bags | laptop bags']").click();
-//            $$(".product-card--name").find(value("Vega"));
-//        });
-//        step("Go Vega Brief page, price should be " + checkPrice, () -> {
-//            $(byText("Vega 2.0 Transit Brief")).click();
-//            $(".desc").shouldHave(text(checkPrice));
-//        });
-//
+        step("CHECK: Checking search results page against search string", () -> {
+            $("div.catalog-list-item__container").shouldNotHave(text(searchString));
+        });
     }
 
 }
